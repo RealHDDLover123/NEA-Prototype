f7e54374858e5ea5dc31fa8439bd51b67762c9d0
RealHDDLover123 <22am3929@barton.ac.uk>
2023-07-05 11:34:11 +0100

    Just started the hacking the console menu for the timer choice

--- NEA Prototype/Program.cs
+++ NEA Prototype/Program.cs
@@ -10,6 +10,50 @@ namespace NEA_Prototype
     {
         static void Main(string[] args)
         {
+
+            bool countdown = false;
+            bool stopwatch = false;
+
+            Console.WriteLine("Do you wish to have a timer (y/n)?");
+            char timerChoice = Console.ReadKey(true).KeyChar;
+
+            if (timerChoice == 'y' || timerChoice == 'Y')
+            {
+                Console.Write("Do you wish to have either a ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("stopwatch");
+                Console.ResetColor();
+                Console.Write(" or a ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("countdown");
+                Console.ResetColor();
+                Console.Write("? To get more information on either, type ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("info");
+                Console.ResetColor();
+
+                string whichTimerChoice = Console.ReadLine();
+
+                if (whichTimerChoice == "info".ToUpper())
+                {
+                    Console.Clear();
+                    Console.WriteLine("The stopwatch");
+                }
+
+                else if (whichTimerChoice == "s")
+                {
+                    stopwatch = true;
+                }
+
+                else if (whichTimerChoice == "c")
+                {
+                    countdown = true;
+                }
+
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
